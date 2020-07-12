360e6d9801eca61e3cbfbd6d461377de2b5e01ca
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-13 00:18:49 +0530

    Added: Test case to check 1 feet equal to 12 inch

--- src/main/java/com/bridgelabz/QuantityMeasurement/service/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/QuantityMeasurement/service/QuantityMeasurement.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.QuantityMeasurement.service;
 
 public class QuantityMeasurement {
+
+      public double quantityMeasurer(InchToFeet inch, double v) {
+            return 0;
+      }
 }
--- src/test/java/com/bridgelabz/QuantityMeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurement/QuantityMeasurementTest.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.QuantityMeasurement;
 
+import com.bridgelabz.QuantityMeasurement.service.InchToFeet;
+import com.bridgelabz.QuantityMeasurement.service.QuantityMeasurement;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 public class QuantityMeasurementTest {
+      QuantityMeasurement quantityMeasurement;
+      double firstValue;
+      double secondValue;
+      @Before
+      public void setUp() {
+            quantityMeasurement = new QuantityMeasurement();
+      }
+      @Test
+      public void given12InchAnd1Feet_IfEqual_ShouldReturnTrue() {
+            firstValue = quantityMeasurement.quantityMeasurer(InchToFeet.INCH, 12.0);
+            secondValue = quantityMeasurement.quantityMeasurer(InchToFeet.FEET, 1.0);
+            System.out.println(firstValue);
+            System.out.println(secondValue);
+            Assert.assertEquals(firstValue, secondValue, 0.0);
+      }
 }
