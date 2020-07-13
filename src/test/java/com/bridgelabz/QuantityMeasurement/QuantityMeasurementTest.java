65a0e8e6638b84b250ae1f0f8dffa44ae2839938
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-13 22:40:45 +0530

    Added: Test case for reference passed to overridden equals method

--- src/test/java/com/bridgelabz/QuantityMeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurement/QuantityMeasurementTest.java
@@ -1,45 +1,52 @@
 package com.bridgelabz.QuantityMeasurement;
+
 import com.bridgelabz.QuantityMeasurement.service.InchToFeet;
 import com.bridgelabz.QuantityMeasurement.service.QuantityMeasurement;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 public class QuantityMeasurementTest {
       QuantityMeasurement quantityMeasurement;
       double firstValue;
       double secondValue;
+
       @Before
       public void setUp() {
             quantityMeasurement = new QuantityMeasurement();
       }
-      @Test
-      public void given0InchAnd0Feet_ShouldReturnTrue() {
-            firstValue = quantityMeasurement.quantityMeasurer(InchToFeet.INCH, 0.0);
-            secondValue = quantityMeasurement.quantityMeasurer(InchToFeet.FEET, 0.0);
-            Assert.assertEquals(firstValue,secondValue,0.0);
-      }
+
       //----------------------------------------Feet--------------------------------------------
       @Test
-      public void givenNullValueForFeet_ShouldReturnFalse() {
+      public void given0Feet_WhenEqual_ShouldReturnTrue() {
+            firstValue = quantityMeasurement.quantityMeasurer(InchToFeet.FEET, 0.0);
+            secondValue = quantityMeasurement.quantityMeasurer(InchToFeet.FEET, 0.0);
+            Assert.assertEquals(firstValue, secondValue, 0.0);
+      }
+
+      @Test
+      public void givenNullValueForFeet_WhenEqual_ShouldReturnFalse() {
             try {
                   quantityMeasurement.quantityMeasurer(InchToFeet.FEET, null);
             } catch (NullPointerException e) {
                   Assert.assertEquals(null, e.getMessage());
             }
       }
+
       @Test
-      public void givenReferenceForFeet_IfEqual_ShouldReturnTrue() {
+      public void givenReferenceForFeet_WhenEqual_ShouldReturnTrue() {
             Assert.assertTrue(quantityMeasurement.equals(quantityMeasurement));
       }
+
       @Test
-      public void givenType_IfEqual_ShouldReturnTrue() {
+      public void givenTypeForFeet_WhenEqual_ShouldReturnTrue() {
             QuantityMeasurement quantityTypeOne = new QuantityMeasurement();
             QuantityMeasurement quantityTypeTwo = new QuantityMeasurement();
             Assert.assertEquals(quantityTypeOne, quantityTypeTwo);
       }
 
       @Test
-      public void givenSameValueForFeet_ShouldReturnTrue() {
+      public void givenSameValueForFeet_WhenEqual_ShouldReturnTrue() {
             firstValue = quantityMeasurement.quantityMeasurer(InchToFeet.FEET, 2.2);
             secondValue = quantityMeasurement.quantityMeasurer(InchToFeet.FEET, 2.2);
             Assert.assertEquals(firstValue, secondValue, 0.0);
@@ -47,11 +54,23 @@ public class QuantityMeasurementTest {
 
       //----------------------------------------Inch--------------------------------------------
       @Test
-      public void givenNullValueForInch_ShouldReturnFalse() {
+      public void given0Inch_WhenEqual_ShouldReturnTrue() {
+            firstValue = quantityMeasurement.quantityMeasurer(InchToFeet.INCH, 0.0);
+            secondValue = quantityMeasurement.quantityMeasurer(InchToFeet.INCH, 0.0);
+            Assert.assertEquals(firstValue, secondValue, 0.0);
+      }
+      @Test
+      public void givenNullValueForInch_WhenEqual_ShouldReturnFalse() {
             try {
                   quantityMeasurement.quantityMeasurer(InchToFeet.INCH, null);
             } catch (NullPointerException e) {
                   Assert.assertEquals(null, e.getMessage());
             }
       }
+
+      @Test
+      public void givenReferenceForInch_WhenEqual_ShouldReturnTrue() {
+            Assert.assertTrue(quantityMeasurement.equals(quantityMeasurement));
+      }
+
 }
