73f90ae7df115d7fbce981c8d94703ca0661b597
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-15 21:59:36 +0530

    Added: Test case to check 1 litre and 1000 ml is equal

--- src/main/java/com/bridgelabz/QuantityMeasurement/utility/UnitType.java
+++ src/main/java/com/bridgelabz/QuantityMeasurement/utility/UnitType.java
@@ -2,7 +2,7 @@ package com.bridgelabz.QuantityMeasurement.utility;
 
 public enum UnitType {
       FEET(12.0), INCH(1.0), YARD(36.0),CENTIMETER(1/2.5),
-      LITRE(1.0),GALLON( 3.78);
+      LITRE(1.0),GALLON( 3.78),MILLILITRE(0.001);
       public double conversionValue;
       UnitType(double conversionValue) {
             this.conversionValue = conversionValue;
