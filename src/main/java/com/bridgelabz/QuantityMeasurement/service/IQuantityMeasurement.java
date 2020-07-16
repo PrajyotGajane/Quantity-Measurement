bb839ca9c78541d5263ee18d114ed1f1330e4e6c
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-16 23:11:01 +0530

    Added: Methods to interface to be impelemented in QuantityMeasurement

--- src/main/java/com/bridgelabz/QuantityMeasurement/service/IQuantityMeasurement.java
+++ src/main/java/com/bridgelabz/QuantityMeasurement/service/IQuantityMeasurement.java
@@ -3,5 +3,9 @@ package com.bridgelabz.QuantityMeasurement.service;
 import com.bridgelabz.QuantityMeasurement.enums.UnitType;
 
 public interface IQuantityMeasurement {
+      double quantityMeasurer(UnitType type, Double valuePassed);
 
+      Double valueAddition(double firstValue, double secondValue);
+
+      Double temperatureConversion(UnitType type, double valuePassed);
 }
